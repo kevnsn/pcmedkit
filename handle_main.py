46feb52edb9b225bb5d6d148079136f01f399a7b
Kevin Sun <kevnsn@gmail.com>
2013-06-01 18:55:22 -0400

    file and data structure

--- handle_main.py
+++ handle_main.py
@@ -1,10 +1,15 @@
 import webapp2
 import home
 app = webapp2.WSGIApplication([('/', home.vol),
-                               ('/volunteer',home.vol),
-                               ('/officer',home.sec),
-                               ('/post/',siteadmin.main),
-                               ('/pcs/',pcv.main),
+                               ('/post/(?P<site>).+/medkit',volunteer.main),
+                               ('/request',volunteer.form),
+                               ('/post/(?P<site>).+',siteadmin.main),
                                ], debug=True)
 
                           
+admin = webapp2.WSGIApplication([('/admin', superadmin.main)], debug=True)
+                          
+siteadmin = webapp2.WSGIApplication([
+                               ('/post/(?P<site>).+/supplyform',siteadmin.form),
+                               ('/post/(?P<site>).+/newmedkit',siteadmin.boxform)
+                               ], debug=True)
