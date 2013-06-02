ae289370c126d9ef6d6aa3b5c0ffcc9413e8b342
Peter Cowan <peterdc@yahoo.com>
2013-06-02 12:02:59 -0400

    point to the right templates

--- volunteer.py
+++ volunteer.py
@@ -16,7 +16,7 @@ class main(webapp2.RequestHandler):
             'position': 'Peace Corps Volunteer',
             'verb': 'extremely enjoy'
         }
-        html = render.page(self, "templates/volunteer_home.html",template_values)
+        html = render.page(self, "templates/volunteer/home.html",template_values)
         self.response.out.write(html)
 
 class form(webapp2.RequestHandler):
@@ -25,5 +25,5 @@ class form(webapp2.RequestHandler):
             'position': 'Medical Officer',
             'verb': 'extremely enjoy'
         }
-        html = render.page(self, "templates/sec_home.html",template_values)
+        html = render.page(self, "templates/volunteer/supply_form.html",template_values)
         self.response.out.write(html)
