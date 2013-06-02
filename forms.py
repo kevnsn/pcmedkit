0ef2c829c2783d7e16b8bb6ce3ccd9099dc5449f
eblahm <matthew.c.halbe@gmail.com>
2013-06-02 13:10:07 -0400

    dynamic form generation

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from models import *
 
-# from google.appengine.ext import db
-# from google.appengine.ext import webapp
-# from google.appengine.ext.webapp import template
-# from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 from google.appengine.ext.db import djangoforms
