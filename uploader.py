8615efbf5c77cce4084e85e6c7c5f8fdd89cfc90
Peter Cowan <peterdc@yahoo.com>
2013-06-02 16:02:15 -0400

    bulk uploader functions 

--- uploader.py
+++ uploader.py
@@ -1,8 +1,9 @@
 
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
-import datetime
+from datetime import datetime
 from models import *
+
 def pl(x):
     if x == [] or x == None:
         return ''
@@ -12,36 +13,65 @@ def pl(x):
 class SupplyUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'Supply',
-            [('date', pl,  ""),
+            [('name', str,  ""),
+            ('description', str,  ""),
+            ('maximum', int,  1),
         ])
 
 class DeliveryEventUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'DeliveryEvent',
-            [('date', pl,  ""),
+            [('name', str,  ""),
+            ('date', datetime.strptime("%m/%d/%Y"),  ""),
+            ('notes', str,  ""),
         ])
 
 class SupplyRequestUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'SupplyRequest',
-            [('date', pl,  ""),
+            [('supplies', list,  []),
+            ('date', datetime.strptime("%m/%d/%Y"),  ""),
+            ('quantities', list,  []),
+            ('delivery_event', str,  ""), # ReferenceProperty
+            ('status', str,  "Requested"),
+            ('status_notes', str,  ""), #TextProperty
+            ('volunteer_notes', str,  ""), #TextProperty
         ])
 
 class VolunteerUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'Volunteer',
-            [('date', pl,  ""),
+            [('first_name', str,  ""),
+            ('last_name', str,  ""),
+            ('phone', str,  ""), #PhoneNumberProperty
+            ('email', str,  ""), #EmailProperty
+            ('trainee_input', str,  ""),
+            ('project', str,  ""),
+            ('cos', datetime.strptime("%m/%d/%Y"),  ""),
+            ('sitelocation', str,  ""), #TextProperty vs StringProperty??
+            ('notes', str,  ""), #TextProperty vs StringProperty??
+            ('medboxs', list,  []),
         ])
 
 class MedBoxUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'MedBox',
-            [('date', pl,  ""),
+            [('code', str,  ""),
+            ('date_issued', datetime.strptime("%m/%d/%Y"),  ""),
+            ('in_use', bool,  False),
+            ('volunteers', list,  []),
+            ('supply_requests', list,  []),
+            ('post_default', list,  []), #ReferenceProperty
         ])
+
 class PostDefaultUploader(bulkloader.Exporter):
     def __init__(self):
         bulkloader.Exporter.__init__(self, 'PostDefault',
-            [('date', pl,  ""),
+            [('slug', str,  ""),
+            ('supplies', list,  []),
+            ('post_admin', list,  []),
+            ('region', str,  ""),
+            ('post', str,  ""),
         ])
 
 exporters = [AlbumExporter]
