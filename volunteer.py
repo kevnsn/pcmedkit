0d104a7099e2ab22516d28a3cba2dbb97edb3495
Kevin Sun <kevnsn@gmail.com>
2013-06-02 13:24:45 -0400

    edited volunteer.py, home.html

--- volunteer.py
+++ volunteer.py
@@ -8,13 +8,14 @@ import models
 import render
 
 import jinja2
-
+import forms
 
 class main(webapp2.RequestHandler):
     def get(self):
         template_values = {
             'position': 'Peace Corps Volunteer',
-            'verb': 'extremely enjoy'
+            'verb': 'extremely enjoy',
+            'formhtml': forms.Volunteer().as_table()  
         }
         html = render.page(self, "templates/volunteer/home.html",template_values)
         self.response.out.write(html)
