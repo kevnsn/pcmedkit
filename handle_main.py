c474aee7d4f74dde48b7716e3ba3c3a99064b962
Matt Halbe <matthew.c.halbe@gmail.com>
2013-06-05 19:31:16 -0400

    Select region on landing page

--- handle_main.py
+++ handle_main.py
@@ -1,17 +1,20 @@
 import webapp2
 import home, superadmin, postadmin, volunteer
 
+
+
 app = webapp2.WSGIApplication([('/',home.landing),
-                               ('/regions',home.landing),
                                (r'/post/(.+)/medkit',volunteer.landing),
                                (r'/post/(.+)/request',volunteer.request_form),
                                (r'/post/(.+)/check_status',volunteer.status),
                                ], debug=True)
 
-admin = webapp2.WSGIApplication([('/admin', superadmin.main)], debug=True)
-
 siteadmin = webapp2.WSGIApplication([(r'/post/(.+)/admin/supplies', postadmin.supply_form),
                                (r'/post/(.+)/admin/requests', postadmin.requests_table),
                                (r'/post/(.+)/admin/assign_medkit', postadmin.medkit),
                                (r'/post/(.+)/admin', postadmin.requests_table),
+                               (r'/post/(.+)', postadmin.requests_table),
                                ], debug=True)
+
+
+admin = webapp2.WSGIApplication([('/admin', superadmin.main)], debug=True)
--- home.py
+++ home.py
@@ -1,15 +1,10 @@
 import webapp2
 import render
+from models import PostDefault
 
 
 class landing(webapp2.RequestHandler):
     def get(self):
-        v = {}
-        html = render.page(self, "templates/landing.html",v)
-        self.response.out.write(html)
-
-class regions(webapp2.RequestHandler):
-    def get(self):
-        v = {}
+        v = {'PostDefault': PostDefault}
         html = render.page(self, "templates/landing.html",v)
         self.response.out.write(html)
